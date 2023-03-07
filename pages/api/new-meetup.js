15dd39f2b4a1466059829ae5f9da6b26cb10f225
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-07 10:48:16 +0100

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
@@ -6,6 +8,22 @@ async function handler(req, res) {
     const data = req.body;
 
     const { title, image, address, discription } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://aman21aa22:EP3S5K6ojaizVpyj@nextjs-course.n7evf2i.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
